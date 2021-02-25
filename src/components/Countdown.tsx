2de125366f054cb9996151ab338c6cfeb07adf56
Rúben Azevedo <ruben.azevedo@bolder.pt>
2021-02-25 00:11:41 +0000

    Component Desafio. ContextProvider, usar o context para completer dados entre componentes como countdown, barra de experiencia, e novo desafio countdown Stop and reset

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,50 +1,87 @@
-import { useState, useEffect } from "react";
-import { setTimeout } from "timers";
+import { useState, useEffect, useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Countdown.module.css";
 
-export function Countdown () {
+let countdownTimeout: NodeJS.Timeout;
 
-    const [time, setTime] = useState(25 * 60);
-    const [active, setActive] = useState(false);
+export function Countdown() {
+  const { startNewChallenge } = useContext(ChallengesContext);
+  const [time, setTime] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
 
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
 
-    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondsLeft, secondsRight] = String(seconds)
+    .padStart(2, "0")
+    .split("");
 
-    const [secondsLeft, secondsRight] = String(seconds).padStart(2, '0').split('');
+  function startCountdown() {
+    setIsActive(true);
+  }
 
-    function startCountdown() {
-        setActive(true);
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setTime(0.1 * 60);
+  }
+
+  // Quando active ou time é modificado
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else if (isActive && time === 0) {
+      console.log("finalizaou");
+      setHasFinished(true);
+      setIsActive(false);
+      startNewChallenge();
     }
-    
-    // Quando active ou time é modificado
-    useEffect(() => {
-        if(active && time > 0 ) {
-            setTimeout(() =>{
-                setTime(time-1);
-            },1000);
-        }
-    }, [active, time]);
+  }, [isActive, time]);
 
-
-    return (
+  return (
+    <div>
+      <div className={styles.countdownContainer}>
         <div>
-            <div className={styles.countdownContainer}>
-                <div>
-                    <span>{minuteLeft}</span>
-                    <span>{minuteRight}</span>
-                </div>
-                <span>:</span>
-                <div>
-                    <span>{secondsLeft}</span>
-                    <span>{secondsRight}</span>
-                </div>
-            </div>
-            
-            <button onClick={startCountdown} type="button" className={styles.countdownButton}>
-                Iniciar um ciclo
-            </button>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
-    );
+        <span>:</span>
+        <div>
+          <span>{secondsLeft}</span>
+          <span>{secondsRight}</span>
+        </div>
+      </div>
+
+      {hasFinished ? (
+        <button disabled className={`${styles.countdownButton}`}>
+          Ciclo encerrado 
+          <img src="check.svg" />
+        </button>
+      ) : (
+        <>
+          {isActive ? (
+            <button
+              onClick={resetCountdown}
+              type="button"
+              className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+            >
+              Abandonar ciclo
+            </button>
+          ) : (
+            <button
+              onClick={startCountdown}
+              type="button"
+              className={`${styles.countdownButton}`}
+            >
+              Iniciar um ciclo
+            </button>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
