ce513feb910303409dbc4092f9b540ef9847da19
RubenJesusAzevedo <ruben.jesus.azevedo@gmail.com>
2021-02-27 12:29:39 +0000

    Deploying to gh-pages from  @ a8ef10f05408edfd52f49eae6fee3acddc2741cb 🚀

---
+++ _next/static/aMGkwvOg_DZ3_pMLogZWF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F4a31754f205f134940a0.css","static\u002Fchunks\u002Fpages\u002Findex-4df840e457b406cab0d8.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d12abf76e3e66686c25d.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
